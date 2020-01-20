650e25f28f81b12f4d209b0181dc954da9230b5e
waldron xing <waldron_xing@163.com>
2020-01-20 17:08:55 +0800

    集成element 和 iview ui demo

---
+++ WIMS-Server/src/main/java/com/moding/wims/wimsserver/WimsServerApplication.java
@@ -0,0 +1,11 @@
+package com.moding.wims.wimsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimsServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WimsServerApplication.class, args);
+    }
+}
---
+++ WIMS-Server/src/test/java/com/moding/wims/winmsserver/WimsServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.moding.wims.winmsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WimsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
